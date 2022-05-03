ed18ce50aa588de4a64058bc095b9ce4dad01c21
longshiplegion <amyers0107@gmail.com>
2022-05-02 21:55:43 -0600

    Stopping Point
    Made a ground,then made a launch and landing pad, then created a rocket. I then created a movement script and made relative movement and added rotation. Made all of those framerate Independant

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+   [SerializeField] float thrust = 100f;
+   [SerializeField] float rotation = 100f;
+   Rigidbody myRidgidBody;
+    // Start is called before the first frame update
+    void Start()
+    {
+       myRidgidBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessThrust();
+        ProcessRotation();
+    }
+
+    void ProcessThrust()
+    {
+        
+        if(Input.GetKey(KeyCode.Space))
+        {
+            myRidgidBody.AddRelativeForce(Vector3.up * thrust * Time.deltaTime);
+        }
+
+    }
+    void ProcessRotation()
+    {
+        if(Input.GetKey(KeyCode.LeftArrow) | Input.GetKey(KeyCode.A))
+        {
+            ApplyRotation(rotation);
+        }
+        else if(Input.GetKey(KeyCode.RightArrow) | Input.GetKey(KeyCode.D))
+        {
+            ApplyRotation(-rotation);
+        } 
+    }
+
+     void ApplyRotation(float rotationThisFrame)
+    {
+        transform.Rotate(Vector3.forward * rotationThisFrame * Time.deltaTime);
+    }
+}
